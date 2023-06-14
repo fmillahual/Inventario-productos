276d8f17a5b9e7e024d96193a3c5962513c9ed2b
fmillahual <fabian.millahual.m@gmail.com>
2023-06-14 17:14:09 -0400

    Se agrega servicio para buscar categoria por ID

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.company.inventory.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -28,6 +30,35 @@ public class CategoryServiceImpl implements ICategoryService{
 			List<Category> category = (List<Category>) categoryDao.findAll();
 			response.getCategoryResponse().setCategory(category);
 			response.setMetadata(HttpStatus.OK.toString(), "Respuesta Exitosa");
+			
+		} catch (Exception e) {
+			response.setMetadata(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		
+		try {
+	
+			Optional<Category> category = categoryDao.findById(id);
+			
+			if(category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata(HttpStatus.OK.toString(), "Categoria encontrada");
+			} else {
+				response.setMetadata(HttpStatus.NOT_FOUND.toString(), "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
 			
 		} catch (Exception e) {
 			response.setMetadata(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "Error al consultar");
